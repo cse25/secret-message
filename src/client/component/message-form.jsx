e99ee172136908cfd95fc74901c33ddba64a51ec
Chris Csekey <chriscsekey@Chriss-MacBook-Pro.local>
2017-06-25 22:14:39 -0700

    add question and answer form fields

--- src/client/component/message-form.jsx
+++ src/client/component/message-form.jsx
@@ -13,6 +13,14 @@ const MessageForm = ({ handleSubmit }: Props) =>
       <label htmlFor="message">Message</label>
       <Field name="message" component="input" type="text" />
     </div>
+    <div>
+      <label htmlFor="question">Question</label>
+      <Field name="question" component="input" type="text" />
+    </div>
+    <div>
+      <label htmlFor="answer">Answer</label>
+      <Field name="answer" component="input" type="text" />
+    </div>
     <button type="submit">Submit</button>
   </form>
 
